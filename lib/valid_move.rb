f14b655572ecefe0449157e1851dde04c511124f
Jason Fournier <jmf4nier@hotmail.com>
2019-04-10 19:00:20 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,8 +1,8 @@
-board = [" ", ]
+board = [" ", " ", "O", " ", " ", "X",]
 
 # code your #valid_move? method here
 def valid_move?(board, index)
-  if position_taken? && board[index].between(1, 9)
+  if position_taken? && board[index].between?(1, 9)
     return true
   else false
   end
